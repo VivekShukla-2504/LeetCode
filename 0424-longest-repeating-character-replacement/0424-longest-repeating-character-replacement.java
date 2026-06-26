5aecc68b34caa60e95d6cdc5ca3943fae2aeb822
Vivek Shukla <7800652515vivek@gmail.com>
2026-06-26 16:59:16 +0530

    Time: 26 ms (27.61%), Space: 47.2 MB (16.39%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,7 +1,7 @@
 class Solution {
     public int characterReplacement(String s, int k) {
      
-        HashMap<Character,Integer> map = new HashMap<>();
+        HashMap<Character,Integer> map = new HashMap<>(26);
         //int[] freq = new int[26];
         int left = 0;
         int ans = 0;
