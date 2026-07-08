2cd518f5c000ae358999d588dba4633dea1cf3dd
Vivek Shukla <7800652515vivek@gmail.com>
2026-07-08 20:00:21 +0530

    Time: 3 ms (4.36%), Space: 46.8 MB (65.16%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,5 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        return IntStream.rangeClosed(1,n).mapToObj(i->i%15==0?"FizzBuzz":i%3==0?"Fizz":i%5==0?"Buzz":""+i).toList();
+    }
+}
