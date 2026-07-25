5777e05d484bca8699e07905c3316ea22d6c9115
Vivek Shukla <7800652515vivek@gmail.com>
2026-07-25 15:36:34 +0530

    Time: 1 ms (99.97%), Space: 42.8 MB (26.71%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,13 +1,10 @@
 class Solution {
     public int reverse(int x) {
-        long result = 0; // use long to safely detect overflow before casting
-
+        long result = 0; 
         while (x != 0) {
             int digit = x % 10;
             x /= 10;
             result = result * 10 + digit;
-
-            // check overflow against int range
             if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
                 return 0;
             }
