422c0b921d264dc73d89a2f57d964e6e3ead6198
Vivek Shukla <7800652515vivek@gmail.com>
2026-07-29 11:33:52 +0530

    Time: 420 ms (6.45%), Space: 280 MB (6.45%) - LeetHub

--- 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.java
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.java
@@ -1,116 +1,238 @@
+
 class Solution {
+
     static final long LIMIT = 1_000_001L;
 
     public String smallestPalindrome(String s, int k) {
+
         int[] freq = new int[26];
-        for (char ch : s.toCharArray()) freq[ch - 'a']++;
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
 
         char middle = 0;
         int[] half = new int[26];
+
         for (int i = 0; i < 26; i++) {
-            if (freq[i] % 2 == 1) middle = (char) ('a' + i);
+
+            if ((freq[i] & 1) == 1)
+                middle = (char) ('a' + i);
+
             half[i] = freq[i] / 2;
         }
 
+
         int n = 0;
-        for (int x : half) n += x;
 
-        int[] primes = sieve(n);               
+        for (int x : half)
+            n += x;
 
-        if (k > countWays(half, n, primes, k)) return "";
+
+        int[] primes = sieve(n);
+        int pCount = primes.length;
+
+
+        int[][] factPow = new int[n + 1][pCount];
+
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 0; j < pCount; j++) {
+
+                int p = primes[j];
+
+                factPow[i][j] = factPow[i - 1][j];
+
+                int x = i;
+
+                while (x % p == 0) {
+                    factPow[i][j]++;
+                    x /= p;
+                }
+            }
+        }
+
+
+        long[] denom = new long[pCount];
+
+
+        for (int i = 0; i < 26; i++) {
+
+            if (half[i] == 0)
+                continue;
+
+            for (int j = 0; j < pCount; j++) {
+                denom[j] += factPow[half[i]][j];
+            }
+        }
+
+
+        long total = countWays(
+                n,
+                factPow,
+                denom,
+                primes,
+                k
+        );
+
+
+        if (total < k)
+            return "";
+
 
         StringBuilder firstHalf = new StringBuilder();
-        long[] numPow    = new long[primes.length];
-        long[] denomBase = new long[primes.length];
+
         int len = n;
 
-        while (len > 0) {
-            int m = 0;
-            while (m < primes.length && primes[m] <= len) m++;
 
-            for (int pi = 0; pi < m; pi++) {
-                int p = primes[pi];
-                numPow[pi] = factPower(len - 1, p);
-                long d = 0;
-                for (int c = 0; c < 26; c++)
-                    if (half[c] > 0) d += factPower(half[c], p);
-                denomBase[pi] = d;
-            }
+        while (len > 0) {
 
             boolean placed = false;
-            for (int c = 0; c < 26 && !placed; c++) {
-                if (half[c] == 0) continue;
 
-                long ways = 1;
-                for (int pi = 0; pi < m; pi++) {
-                    int p = primes[pi];
-                    long vp = valuation(half[c], p);
-                    long power = numPow[pi] - denomBase[pi] + vp;
-                    while (power-- > 0) {
-                        ways *= p;
-                        if (ways >= LIMIT) { ways = LIMIT; break; }
-                    }
-                    if (ways >= LIMIT) break;
+
+            for (int c = 0; c < 26; c++) {
+
+                if (half[c] == 0)
+                    continue;
+
+
+                int old = half[c];
+
+
+                half[c]--;
+
+
+                for (int j = 0; j < pCount; j++) {
+
+                    denom[j] -= factPow[old][j] - factPow[old - 1][j];
+
                 }
 
+
+                long ways = countWays(
+                        len - 1,
+                        factPow,
+                        denom,
+                        primes,
+                        k
+                );
+
+
                 if (k > ways) {
+
                     k -= ways;
-                } else {
-                    half[c]--;
+
+
+                    half[c]++;
+
+                    for (int j = 0; j < pCount; j++) {
+
+                        denom[j] += factPow[old][j] - factPow[old - 1][j];
+
+                    }
+
+                } 
+                else {
+
                     firstHalf.append((char) ('a' + c));
                     placed = true;
+                    break;
+
                 }
             }
 
-            if (!placed) return "";
+
+            if (!placed)
+                return "";
+
+
             len--;
         }
 
+
         StringBuilder ans = new StringBuilder(firstHalf);
-        if (middle != 0) ans.append(middle);
+
+        if (middle != 0)
+            ans.append(middle);
+
         ans.append(firstHalf.reverse());
+
+
         return ans.toString();
     }
 
-    private long countWays(int[] freq, int n, int[] primes, long limit) {
+
+
+    private long countWays(
+            int n,
+            int[][] factPow,
+            long[] denom,
+            int[] primes,
+            int limit
+    ) {
+
         long ans = 1;
-        for (int p : primes) {
-            if (p > n) break;
-            long power = factPower(n, p);
-            for (int x : freq) power -= factPower(x, p);
+
+
+        for (int j = 0; j < primes.length; j++) {
+
+            long power = factPow[n][j] - denom[j];
+
+
             while (power-- > 0) {
-                ans *= p;
-                if (ans >= limit) return limit;
+
+                ans *= primes[j];
+
+
+                if (ans >= limit)
+                    return LIMIT;
             }
         }
+
+
         return ans;
     }
 
-    private long factPower(int n, int p) {
-        long c = 0;
-        while (n > 0) { n /= p; c += n; }
-        return c;
-    }
 
-    private long valuation(int x, int p) {
-        long c = 0;
-        while (x % p == 0) { x /= p; c++; }
-        return c;
-    }
 
     private int[] sieve(int n) {
-        if (n < 2) return new int[0];
-        boolean[] comp = new boolean[n + 1];
-        int cnt = 0;
+
+        if (n < 2)
+            return new int[0];
+
+
+        boolean[] composite = new boolean[n + 1];
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+
         for (int i = 2; i <= n; i++) {
-            if (!comp[i]) {
-                cnt++;
-                for (long j = (long) i * i; j <= n; j += i) comp[(int) j] = true;
+
+            if (!composite[i]) {
+
+                list.add(i);
+
+
+                if ((long)i * i <= n) {
+
+                    for (long j = (long)i * i; j <= n; j += i) {
+                        composite[(int)j] = true;
+                    }
+                }
             }
         }
-        int[] primes = new int[cnt];
-        int idx = 0;
-        for (int i = 2; i <= n; i++) if (!comp[i]) primes[idx++] = i;
+
+
+        int[] primes = new int[list.size()];
+
+
+        for (int i = 0; i < primes.length; i++) {
+            primes[i] = list.get(i);
+        }
+
+
         return primes;
     }
 }
