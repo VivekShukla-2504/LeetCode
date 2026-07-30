63b508a68138319c1e751fb016ee4f6afeac4463
Vivek Shukla <7800652515vivek@gmail.com>
2026-07-30 18:47:27 +0530

    Time: 48 ms (31.94%), Space: 47.6 MB (51.71%) - LeetHub

--- 0913-cat-and-mouse/0913-cat-and-mouse.java
+++ 0913-cat-and-mouse/0913-cat-and-mouse.java
@@ -1,91 +1,91 @@
-
 class Solution {
-    private static final int MOUSE_TURN = 1;
-    private static final int CAT_TURN = 2;
 
     private static final int DRAW = 0;
-    private static final int MOUSE_WIN = 1;
-    private static final int CAT_WIN = 2;
+    private static final int MOUSE = 1;
+    private static final int CAT = 2;
 
     public int catMouseGame(int[][] graph) {
         int n = graph.length;
 
-        int[][][] color = new int[n][n][3];
-
-        int[][][] degree = new int[n][n][3];
+        int[][][] color = new int[n][n][2];
+        int[][][] degree = new int[n][n][2];
 
         for (int m = 0; m < n; m++) {
-            for (int c = 1; c < n; c++) {
-                degree[m][c][MOUSE_TURN] = graph[m].length;
-                degree[m][c][CAT_TURN] = graph[c].length;
+            for (int c = 0; c < n; c++) {
+                degree[m][c][0] = graph[m].length;
+                degree[m][c][1] = graph[c].length;
 
-                for (int node : graph[c]) {
-                    if (node == 0) {
-                        degree[m][c][CAT_TURN]--;
-                        break;
+                for (int next : graph[c]) {
+                    if (next == 0) {
+                        degree[m][c][1]--;
                     }
                 }
             }
         }
 
-        Queue<int[]> queue = new ArrayDeque<>();
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int c = 1; c < n; c++) {
+            color[0][c][0] = MOUSE;
+            color[0][c][1] = MOUSE;
+            queue.offer(new int[]{0, c, 0, MOUSE});
+            queue.offer(new int[]{0, c, 1, MOUSE});
+        }
 
         for (int i = 1; i < n; i++) {
-            for (int t = 1; t <= 2; t++) {
-                color[0][i][t] = MOUSE_WIN;
-                queue.offer(new int[]{0, i, t, MOUSE_WIN});
-
-                color[i][i][t] = CAT_WIN;
-                queue.offer(new int[]{i, i, t, CAT_WIN});
-            }
+            color[i][i][0] = CAT;
+            color[i][i][1] = CAT;
+            queue.offer(new int[]{i, i, 0, CAT});
+            queue.offer(new int[]{i, i, 1, CAT});
         }
 
         while (!queue.isEmpty()) {
-            int[] state = queue.poll();
-            int m = state[0];
-            int c = state[1];
-            int t = state[2];
-            int res = state[3];
+            int[] cur = queue.poll();
+            int m = cur[0];
+            int c = cur[1];
+            int turn = cur[2];
+            int result = cur[3];
 
-            if (m == 1 && c == 2 && t == MOUSE_TURN) {
-                return res;
-            }
+            for (int[] parent : parents(graph, m, c, turn)) {
+                int pm = parent[0];
+                int pc = parent[1];
+                int pTurn = parent[2];
 
-            int prevTurn = (t == MOUSE_TURN) ? CAT_TURN : MOUSE_TURN;
+                if (color[pm][pc][pTurn] != DRAW) continue;
 
-            if (prevTurn == MOUSE_TURN) {
-                for (int prevM : graph[m]) {
-                    if (color[prevM][c][MOUSE_TURN] != DRAW) continue;
+                if ((pTurn == 0 && result == MOUSE) ||
+                    (pTurn == 1 && result == CAT)) {
 
-                    if (res == MOUSE_WIN) {
-                        color[prevM][c][MOUSE_TURN] = MOUSE_WIN;
-                        queue.offer(new int[]{prevM, c, MOUSE_TURN, MOUSE_WIN});
-                    } else {
-                        degree[prevM][c][MOUSE_TURN]--;
-                        if (degree[prevM][c][MOUSE_TURN] == 0) {
-                            color[prevM][c][MOUSE_TURN] = CAT_WIN;
-                            queue.offer(new int[]{prevM, c, MOUSE_TURN, CAT_WIN});
-                        }
-                    }
-                }
-            } else {
-                for (int prevC : graph[c]) {
-                    if (prevC == 0 || color[m][prevC][CAT_TURN] != DRAW) continue;
-
-                    if (res == CAT_WIN) {
-                        color[m][prevC][CAT_TURN] = CAT_WIN;
-                        queue.offer(new int[]{m, prevC, CAT_TURN, CAT_WIN});
-                    } else {
-                        degree[m][prevC][CAT_TURN]--;
-                        if (degree[m][prevC][CAT_TURN] == 0) {
-                            color[m][prevC][CAT_TURN] = MOUSE_WIN;
-                            queue.offer(new int[]{m, prevC, CAT_TURN, MOUSE_WIN});
-                        }
+                    color[pm][pc][pTurn] = result;
+                    queue.offer(new int[]{pm, pc, pTurn, result});
+                } else {
+                    degree[pm][pc][pTurn]--;
+                    if (degree[pm][pc][pTurn] == 0) {
+                        int lose = (pTurn == 0) ? CAT : MOUSE;
+                        color[pm][pc][pTurn] = lose;
+                        queue.offer(new int[]{pm, pc, pTurn, lose});
                     }
                 }
             }
         }
 
-        return color[1][2][MOUSE_TURN];
+        return color[1][2][0];
+    }
+
+    private List<int[]> parents(int[][] graph, int mouse, int cat, int turn) {
+        List<int[]> res = new ArrayList<>();
+
+        if (turn == 0) {
+            for (int prevCat : graph[cat]) {
+                if (prevCat == 0) continue;
+                res.add(new int[]{mouse, prevCat, 1});
+            }
+        } else {
+            for (int prevMouse : graph[mouse]) {
+                res.add(new int[]{prevMouse, cat, 0});
+            }
+        }
+
+        return res;
     }
 }
