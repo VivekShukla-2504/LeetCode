54e4c0664dfe096237cae48d35b46b3547cbb307
Vivek Shukla <7800652515vivek@gmail.com>
2026-07-30 18:35:52 +0530

    Time: 28 ms (54.03%), Space: 71.5 MB (73.17%) - LeetHub

---
+++ 0546-remove-boxes/0546-remove-boxes.java
@@ -0,0 +1,42 @@
+class Solution {
+    private int[][][] memo;
+
+    public int removeBoxes(int[] boxes) {
+        int n = boxes.length;
+        memo = new int[n][n][n];
+        return calculatePoints(boxes, 0, n - 1, 0);
+    }
+
+    private int calculatePoints(int[] boxes, int i, int j, int k) {
+        if (i > j) {
+            return 0;
+        }
+
+        if (memo[i][j][k] > 0) {
+            return memo[i][j][k];
+        }
+
+        // Optimization: Compress contiguous boxes of the same color starting at index i
+        int originalI = i;
+        int originalK = k;
+        while (i + 1 <= j && boxes[i] == boxes[i + 1]) {
+            i++;
+            k++;
+        }
+
+        // Option 1: Remove boxes[i] and all contiguous/preceding matching boxes immediately
+        int maxPoints = (k + 1) * (k + 1) + calculatePoints(boxes, i + 1, j, 0);
+
+        // Option 2: Look for another box with the same color further to the right
+        for (int m = i + 1; m <= j; m++) {
+            if (boxes[m] == boxes[i]) {
+                int currentPoints = calculatePoints(boxes, i + 1, m - 1, 0) 
+                                  + calculatePoints(boxes, m, j, k + 1);
+                maxPoints = Math.max(maxPoints, currentPoints);
+            }
+        }
+
+        memo[originalI][j][originalK] = maxPoints;
+        return maxPoints;
+    }
+}
