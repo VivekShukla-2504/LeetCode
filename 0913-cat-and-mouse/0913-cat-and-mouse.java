053dba708a23c6a977859054d332ac82467f03a0
Vivek Shukla <7800652515vivek@gmail.com>
2026-07-30 18:44:01 +0530

    Time: 16 ms (98.86%), Space: 47.3 MB (73.38%) - LeetHub

--- 0913-cat-and-mouse/0913-cat-and-mouse.java
+++ 0913-cat-and-mouse/0913-cat-and-mouse.java
@@ -10,20 +10,15 @@ class Solution {
     public int catMouseGame(int[][] graph) {
         int n = graph.length;
 
-        // color[m][c][t] stores the outcome of the state (m, c, t)
-        // 0: DRAW (default), 1: MOUSE_WIN, 2: CAT_WIN
         int[][][] color = new int[n][n][3];
 
-        // degree[m][c][t] stores the number of available moves for the current player
         int[][][] degree = new int[n][n][3];
 
-        // Precompute degrees for all states
         for (int m = 0; m < n; m++) {
             for (int c = 1; c < n; c++) {
                 degree[m][c][MOUSE_TURN] = graph[m].length;
                 degree[m][c][CAT_TURN] = graph[c].length;
 
-                // Cat cannot move to Node 0 (Hole)
                 for (int node : graph[c]) {
                     if (node == 0) {
                         degree[m][c][CAT_TURN]--;
@@ -33,24 +28,18 @@ class Solution {
             }
         }
 
-        // Queue for BFS backward propagation
-        // Array format: {mousePos, catPos, turn, result}
         Queue<int[]> queue = new ArrayDeque<>();
 
-        // 1. Initialize Terminal States
         for (int i = 1; i < n; i++) {
             for (int t = 1; t <= 2; t++) {
-                // Mouse reached hole -> Mouse Wins
                 color[0][i][t] = MOUSE_WIN;
                 queue.offer(new int[]{0, i, t, MOUSE_WIN});
 
-                // Cat caught Mouse -> Cat Wins
                 color[i][i][t] = CAT_WIN;
                 queue.offer(new int[]{i, i, t, CAT_WIN});
             }
         }
 
-        // 2. Backward BFS Propagation
         while (!queue.isEmpty()) {
             int[] state = queue.poll();
             int m = state[0];
@@ -58,24 +47,20 @@ class Solution {
             int t = state[2];
             int res = state[3];
 
-            // If we reached the initial state, return the calculated result early
             if (m == 1 && c == 2 && t == MOUSE_TURN) {
                 return res;
             }
 
-            // Find all predecessor states (states that could lead into the current state)
             int prevTurn = (t == MOUSE_TURN) ? CAT_TURN : MOUSE_TURN;
 
             if (prevTurn == MOUSE_TURN) {
                 for (int prevM : graph[m]) {
                     if (color[prevM][c][MOUSE_TURN] != DRAW) continue;
 
-                    // If Mouse can move to a state where Mouse Wins
                     if (res == MOUSE_WIN) {
                         color[prevM][c][MOUSE_TURN] = MOUSE_WIN;
                         queue.offer(new int[]{prevM, c, MOUSE_TURN, MOUSE_WIN});
                     } else {
-                        // Res == CAT_WIN: Decrement available moves for Mouse
                         degree[prevM][c][MOUSE_TURN]--;
                         if (degree[prevM][c][MOUSE_TURN] == 0) {
                             color[prevM][c][MOUSE_TURN] = CAT_WIN;
@@ -83,16 +68,14 @@ class Solution {
                         }
                     }
                 }
-            } else { // prevTurn == CAT_TURN
+            } else {
                 for (int prevC : graph[c]) {
                     if (prevC == 0 || color[m][prevC][CAT_TURN] != DRAW) continue;
 
-                    // If Cat can move to a state where Cat Wins
                     if (res == CAT_WIN) {
                         color[m][prevC][CAT_TURN] = CAT_WIN;
                         queue.offer(new int[]{m, prevC, CAT_TURN, CAT_WIN});
                     } else {
-                        // Res == MOUSE_WIN: Decrement available moves for Cat
                         degree[m][prevC][CAT_TURN]--;
                         if (degree[m][prevC][CAT_TURN] == 0) {
                             color[m][prevC][CAT_TURN] = MOUSE_WIN;
