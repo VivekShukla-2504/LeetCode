c4b89aa9c176aab0bfe9180e553c12feea52f168
Vivek Shukla <7800652515vivek@gmail.com>
2026-08-05 11:46:30 +0530

    Time: 74 ms (56.73%), Space: 274.5 MB (93.27%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] inv : invocations) {
+            graph.get(inv[0]).add(inv[1]);
+        }
+
+        boolean[] isSuspicious = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(k);
+        isSuspicious[k] = true;
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            for (int v : graph.get(u)) {
+                if (!isSuspicious[v]) {
+                    isSuspicious[v] = true;
+                    queue.offer(v);
+                }
+            }
+        }
+
+        for (int[] inv : invocations) {
+            int u = inv[0];
+            int v = inv[1];
+            if (!isSuspicious[u] && isSuspicious[v]) {
+                List<Integer> allMethods = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    allMethods.add(i);
+                }
+                return allMethods;
+            }
+        }
+
+        List<Integer> remaining = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!isSuspicious[i]) {
+                remaining.add(i);
+            }
+        }
+        return remaining;
+    }
+}
