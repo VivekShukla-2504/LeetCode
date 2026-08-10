44287ab346793c42298a0ec640b43ac79412d08b
Vivek Shukla <7800652515vivek@gmail.com>
2026-08-10 10:35:53 +0530

    Time: 14 ms (91.38%), Space: 42.3 MB (83.55%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean winnerSquareGame(int n) {
-        boolean[] dp = new boolean[n+1];
+        boolean[] dp = new boolean[n + 1];
 
-        for(int i = 1;i<=n;i++)
-        {
-            for(int k = 1;k*k <=i;k++)
-            {
-                if(!dp[i-k*k])
-                {
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k * k <= i; k++) {
+
+                
+                if (!dp[i - k * k]) {
                     dp[i] = true;
                     break;
                 }
             }
         }
+
         return dp[n];
     }
 }
