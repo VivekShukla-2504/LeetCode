efc0fa00049d21ca127bdcd2a2e4dfb9e2efc2fd
Vivek Shukla <7800652515vivek@gmail.com>
2026-08-22 15:25:48 +0530

    Time: 0 ms (100%), Space: 42.4 MB (42.42%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkDivisibility(int n) 
+    {
+        int x = 0;
+        int y = 1;
+        int ori = n;
+        if(n<=0)
+        {
+            return false;
+        }
+        while(n>0)
+        {
+             x= x+(n%10);
+             y= y*(n%10);
+             n = n/10;
+        }
+        
+        return ori % (x+y) == 0;
+    }
+}
